7030c0c381722d67b7b4915dca11ffb1238e25ff
SettingDust <settingdust@gmail.com>
2024-06-17 19:13:33 +0800

    feat: add kotlin forge to neoforge

--- neoforge.gradle.kts
+++ neoforge.gradle.kts
@@ -4,12 +4,43 @@ pluginManagement {
     }
 }
 
+val minecraft: String by settings.extra
+val kotlin: String? by settings.extra
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://thedarkcolour.github.io/KotlinForForge/") {
+            content {
+                includeGroup("thedarkcolour")
+            }
+        }
+        maven("https://maven.neoforged.net/releases")
+    }
+}
+
 dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
     plugin("neoforge-gradle", "net.neoforged.gradle.userdev").version("7.+")
     plugin("neoforge-gradle-vanilla", "net.neoforged.gradle.vanilla").version("7.+")
     plugin("neoforge-gradle-mixin", "net.neoforged.gradle.mixin").version("7.+")
 
-    // https://linkie.shedaniel.dev/dependencies?loader=neoforge
-    library("neoforge", "net.neoforged", "neoforge").version("21.0.14-beta")
-}
 
+    val neoforgeVersions = mapOf(
+        "1.21" to "21.0.14-beta"
+    )
+    // https://linkie.shedaniel.dev/dependencies?loader=neoforge
+    library("neoforge", "net.neoforged", "neoforge").version(neoforgeVersions.getOrElse(minecraft) {
+        error("Unknown minecraft version $minecraft to get neoforge version")
+    })
+
+    if (kotlin != null) {
+        // https://modrinth.com/mod/kotlin-for-forge/versions
+        val kotlinForgeVersions = mapOf(
+            "1.21" to "5.3.0"
+        )
+        library("kotlin-neoforge", "thedarkcolour", "kotlinforforge-neoforge").version(
+            kotlinForgeVersions.getOrElse(
+                minecraft
+            ) { error("Unknown minecraft version $minecraft to get Kotlin For Forge version") })
+    }
+}

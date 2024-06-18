8e7beafe58f975471098ac529c000f15bd982d4b
SettingDust <settingdust@gmail.com>
2024-06-18 12:15:48 +0800

    feat(mods): split mods into mods

--- fabric.gradle.kts
+++ fabric.gradle.kts
@@ -58,16 +58,4 @@ dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
                 }+kotlin.$kotlin"
             )
     }
-
-    // https://modrinth.com/mod/modmenu/versions
-    val modmenuVersions = mapOf(
-        "1.21" to "11.0.0-rc.4"
-    )
-    library(
-        "modmenu",
-        "com.terraformersmc",
-        "modmenu"
-    ).version(modmenuVersions.getOrElse(minecraft) {
-        error("Unknown minecraft version $minecraft to get modmenu version")
-    })
 }
---
+++ mods.gradle.kts
@@ -0,0 +1,13 @@
+dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
+    // https://modrinth.com/mod/modmenu/versions
+    val modmenuVersions = mapOf(
+        "1.21" to "11.0.0-rc.4"
+    )
+    library(
+        "modmenu",
+        "com.terraformersmc",
+        "modmenu"
+    ).version(modmenuVersions.getOrElse(minecraft) {
+        error("Unknown minecraft version $minecraft to get modmenu version")
+    })
+}

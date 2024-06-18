6c3993204aa3c31ddad6c4cef5094e612b013c11
SettingDust <settingdust@gmail.com>
2024-06-18 22:15:21 +0800

    chore(vanillagradle): remove mixin

--- vanillagradle.gradle.kts
+++ vanillagradle.gradle.kts
@@ -12,6 +12,4 @@ dependencyResolutionManagement {
 
 dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
     plugin("vanilla-gradle", "org.spongepowered.gradle.vanilla").version("0.2.1-SNAPSHOT")
-    // VG need to add mixin manually
-    library("mixin", "org.spongepowered", "mixin").version("0.8.5")
 }

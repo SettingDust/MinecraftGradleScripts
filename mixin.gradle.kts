2b0ae8b7eac8245bf32f608c42ee0019ef22157d
SettingDust <settingdust@gmail.com>
2024-06-27 14:13:22 +0800

    feat(mixin): add mixinsquared

--- mixin.gradle.kts
+++ mixin.gradle.kts
@@ -10,4 +10,10 @@ dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
     library("mixinextras-common", "io.github.llamalad7", "mixinextras-common").version(mixinextras)
     library("mixinextras-neoforge", "io.github.llamalad7", "mixinextras-neoforge").version(mixinextras)
     library("mixinextras-fabric", "io.github.llamalad7", "mixinextras-fabric").version(mixinextras)
+
+    val mixinsquared = "0.2.0-beta.3"
+    library("mixinsquared-common", "com.github.bawnorton.mixinsquared", "mixinsquared-common").version(mixinsquared)
+    library("mixinsquared-fabric", "com.github.bawnorton.mixinsquared", "mixinsquared-fabric").version(mixinsquared)
+    library("mixinsquared-neoforge", "com.github.bawnorton.mixinsquared", "mixinsquared-neoforge").version(mixinsquared)
+    library("mixinsquared-forge", "com.github.bawnorton.mixinsquared", "mixinsquared-forge").version(mixinsquared)
 }

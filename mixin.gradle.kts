d4d04649658694158327d745bd387480964d4429
SettingDust <settingdust@gmail.com>
2024-07-16 12:45:43 +0800

    feat(mixin): add mixin gradle & bump versions

--- mixin.gradle.kts
+++ mixin.gradle.kts
@@ -1,24 +1,29 @@
 dependencyResolutionManagement {
     repositories {
-        maven("https://jitpack.io") {
-            content {
-                includeGroupAndSubgroups("com.github")
-            }
+        maven("https://maven.bawnorton.com") {
+            content { includeGroupAndSubgroups("com.bawnorton.mixinsquared") }
         }
         maven("https://repo.spongepowered.org/repository/maven-public/")
     }
 }
 
 dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
-    library("mixin", "org.spongepowered", "mixin").version("0.8.5")
-    val mixinextras = "0.3.6"
+    plugin("mixin", "org.spongepowered.mixin").version("0.7-SNAPSHOT")
+
+    library("mixin", "org.spongepowered", "mixin").version("0.8.7")
+    val mixinextras = "0.4.0"
     library("mixinextras-common", "io.github.llamalad7", "mixinextras-common").version(mixinextras)
-    library("mixinextras-neoforge", "io.github.llamalad7", "mixinextras-neoforge").version(mixinextras)
+    library("mixinextras-neoforge", "io.github.llamalad7", "mixinextras-neoforge")
+        .version(mixinextras)
     library("mixinextras-fabric", "io.github.llamalad7", "mixinextras-fabric").version(mixinextras)
 
-    val mixinsquared = "0.2.0-beta.3"
-    library("mixinsquared-common", "com.github.bawnorton.mixinsquared", "mixinsquared-common").version(mixinsquared)
-    library("mixinsquared-fabric", "com.github.bawnorton.mixinsquared", "mixinsquared-fabric").version(mixinsquared)
-    library("mixinsquared-neoforge", "com.github.bawnorton.mixinsquared", "mixinsquared-neoforge").version(mixinsquared)
-    library("mixinsquared-forge", "com.github.bawnorton.mixinsquared", "mixinsquared-forge").version(mixinsquared)
+    val mixinsquared = "0.2.0-beta.6"
+    library("mixinsquared-common", "com.bawnorton.mixinsquared", "mixinsquared-common")
+        .version(mixinsquared)
+    library("mixinsquared-fabric", "com.bawnorton.mixinsquared", "mixinsquared-fabric")
+        .version(mixinsquared)
+    library("mixinsquared-neoforge", "com.bawnorton.mixinsquared", "mixinsquared-neoforge")
+        .version(mixinsquared)
+    library("mixinsquared-forge", "com.bawnorton.mixinsquared", "mixinsquared-forge")
+        .version(mixinsquared)
 }

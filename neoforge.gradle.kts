dc6f6c09dd0177b456eb911aeae79309f111726d
SettingDust <settingdust@gmail.com>
2024-09-15 21:23:35 +0800

    chore(neoforge): update neoforge

--- neoforge.gradle.kts
+++ neoforge.gradle.kts
@@ -26,7 +26,7 @@ dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
 
     val neoforgeVersions = mapOf(
         "1.21" to "21.0.167",
-        "1.21.1" to "21.1.12"
+        "1.21.1" to "21.1.51"
     )
     // https://linkie.shedaniel.dev/dependencies?loader=neoforge
     library("neoforge", "net.neoforged", "neoforge").version(neoforgeVersions.getOrElse(minecraft) {

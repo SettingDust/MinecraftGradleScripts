cf532e9d01325a1316840d1ae3978354f5b51b30
SettingDust <settingdust@gmail.com>
2024-09-18 17:54:54 +0800

    chore(quilt): bump quilt loom

--- quilt.gradle.kts
+++ quilt.gradle.kts
@@ -6,6 +6,6 @@ pluginManagement {
 
 // https://quiltmc.org/en/usage/latest-versions/
 dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
-    library("quilt-loader", "org.quiltmc", "quilt-loader").version("0.26.0")
-    plugin("quilt-loom", "org.quiltmc.loom").version("1.6.+")
+    library("quilt-loader", "org.quiltmc", "quilt-loader").version("0.26.4")
+    plugin("quilt-loom", "org.quiltmc.loom").version("1.7.+")
 }

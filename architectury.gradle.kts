bc3e963eb6cc414d561e6a9a6c9373794fe2a086
SettingDust <settingdust@gmail.com>
2024-10-15 11:56:33 +0800

    feat(architectury)：add architectury api versions

--- architectury.gradle.kts
+++ architectury.gradle.kts
@@ -13,4 +13,11 @@ dependencyResolutionManagement.versionCatalogs.maybeCreate("catalog").apply {
     library("architectury-fabric", "dev.architectury", "architectury-fabric").version(architecturyVersion)
     library("architectury-forge", "dev.architectury", "architectury-forge").version(architecturyVersion)
     library("architectury-neoforge", "dev.architectury", "architectury-neoforge").version(architecturyVersion)
+
+    for (version in architecturyVersions) {
+        library("architectury-${version.key}", "dev.architectury", "architectury").version(version.value)
+        library("architectury-fabric-${version.key}", "dev.architectury", "architectury-fabric").version(version.value)
+        library("architectury-forge-${version.key}", "dev.architectury", "architectury-forge").version(version.value)
+        library("architectury-neoforge-${version.key}", "dev.architectury", "architectury-neoforge").version(version.value)
+    }
 }
